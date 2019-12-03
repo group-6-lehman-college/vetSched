80faa4ed742e246d6161b4447398dcb8d4d947a9
alixlm19 <alixlm19@hotmail.com>
2019-12-03 18:53:17 -0500

    Added login/registration backend

---
+++ backend/loginBackend.php
@@ -0,0 +1,40 @@
+<?php
+    require_once("dbConnection.php");
+
+    $db = Database::getConnection();
+    //Fetch the user information
+    $email = $_POST['email'];           
+    $password = $_POST['password'];  
+
+    //Validate user email
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email format is valid</br>";
+    }
+
+    $result = $db->query("SELECT id, hash, salt FROM entity WHERE email = '$email'");       //Get the user's hash and salt
+
+    //echo var_dump($result);
+    //FIXME verify hash and authenticate
+    if($result) {
+        $row  = MySQLi_fetch_row($result);                                          //Get the selected row from the result query
+        $id   = $row[0];                                                            //Get the user id
+        $hash = "\$argon2id\$v=19\$m=65536,t=4,p=1\$$row[2]\$$row[1]";              //Get the hash from the result query
+
+        //Verify if the password matches the hash
+        if(password_verify($password, $hash)) {
+            session_start();                                //Start the session
+            //Generate session CSRF token
+            if (empty($_SESSION["token"])) {
+                $_SESSION["token"] = bin2hex(random_bytes(32));                         //Set session token
+            }
+            $token = $_SESSION["token"];  
+            $_SESSION["status"] = "Active";   
+            echo "as;dlfkj";                                     
+            header("Location: dashboard.php");
+        } else {
+            header("Location: login.php");
+        }
+    } else {
+        echo "User does not exist</br>";    
+    }
+?>
---
+++ backend/registerBackend.php
@@ -0,0 +1,70 @@
+<?php
+    require_once('dbConnection.php');
+
+    
+    //Connect to the database
+    $db = Database::getConnection();
+
+    //End the program if there was a connection error
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    echo "Connected successfully</br>";
+
+    //Fetch the user information
+    $first_name = $_POST["first_name"];
+    $middle_name = $_POST["middle_name"];
+    $last_name  = $_POST["last_name"];
+    $email      = $_POST["email"];
+    $password1  = $_POST["password1"];
+    $password2  = $_POST["password2"];
+
+    if(!isset($middle_name)) {
+        $middle_name = "";
+    }
+
+    //Check if every field was passed by the user
+    if(!isset($first_name, $last_name, $email, $password1, $password2)) {
+        echo "Missing information</br>";
+    }
+
+    //Validate user email
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email is valid</br>";
+    }
+
+    //Compare submitted passwords (TEMPORARY)
+    if($password1 == $password1) {
+        $hash = password_hash($password1, PASSWORD_ARGON2ID);
+        
+        $hash_array = explode('$', $hash);//b echo print_r($hash_array, true) . "</br>";
+        $salt = $hash_array[4];
+        $pwd_hash = $hash_array[5];
+        echo "length" . strlen($pwd_hash) . "</br>";
+    }
+    
+    $sql = " INSERT INTO entity (first_name, middle_name, last_name, email, hash, salt) 
+    VALUES ('$first_name', '$middle_name', '$last_name', '$email', '$pwd_hash', '$salt') ";
+
+    //Query the db
+    if($db->query($sql) === TRUE) {
+        
+        $id = $db->insert_id;
+        $db->query(" INSERT INTO user (entity_id, missed_appointment) VALUES ('$id', '0') ");
+        session_start();    
+
+        //Generate session CSRF token
+        if (empty($_SESSION["token"])) {
+            $_SESSION["token"] = bin2hex(random_bytes(32));                         //Set session token
+        }
+        $token = $_SESSION["token"];  
+        $_SESSION["status"] = "Active";                                              
+        header("Location: dashboard.php");
+
+    } else {
+        echo "Error: " . $sql . "<br>" . $db->error;
+    }
+
+                                                                                 
+?>
