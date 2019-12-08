3b5ed028f4d8c817fcb1101ea6de318dbe39c23c
alixlm19 <alixlm19@hotmail.com>
2019-12-08 04:18:28 -0500

    Pet backend now inserts to database

--- backend/addPet.php
+++ backend/addPet.php
@@ -1,11 +1,34 @@
 <?php
     require_once("dbConnection.php");
-
+    
     $db = Database::getConnection();
+    session_start();
 
-    $name = $_POST['name'];
-    $type = $_POST['type'];
-    $sex  = $_POST['sex'];
+    $id = $_SESSION["id"];
+    $result = $db->query(" SELECT first_name, last_name FROM entity WHERE id = '$id' ");
 
-    echo $name . "</br>" . $type . "</br>" . $sex;
+    $row  = MySQLi_fetch_row($result); 
+    $first_name = $row[0];
+    $last_name = $row[1];
+    echo "Logged in as: $first_name $last_name</br>";
+
+    
+    $name        = $_POST['name'];
+    $sex         = $_POST['sex'];
+    $type        = $_POST['type'];
+    $description = $_POST['description'];
+    
+    echo $id . "</br>" . $name . "</br>" . $type . "</br>" . $sex
+    . "</br>" . $description;
+
+    $fields = "user_entity_id, name, sex, type, description";
+    $values = " '$name', '$sex', '$type', '$description' ";
+    $query = " INSERT INTO pet ($fields) VALUES ($values) ";
+    $result = $db->query($query);
+
+    if(!$result) {
+        echo "Error adding new pet :(</br>" . $db->error;
+    } else {
+        echo "Pet added successfully! :)";
+    }
 ?>
