c9f1df743236380d768cd960c49212af2d69a731
alixlm19 <alixlm19@hotmail.com>
2019-12-08 17:06:34 -0500

    Added debug mode

--- backend/checkSession.php
+++ backend/checkSession.php
@@ -1,22 +1,31 @@
 <?php
+    include_once("config.php");
     session_start();
     
-    if(!isset($_SESSION['token'])) {
+    if(!isset($_SESSION['id'])) {
         header("Location: ../login.php");
     }
-    
-    if(isset($_SESSION['is_user'])) {
 
-        echo "<script>alert('this is a user');</script>";
-    }
 
-    if(isset($_SESSION['is_admin'])) {
+    //Check if developer mode is ON
+    //If so, display user inforamtion, such as privileges, id, etc.
+    if($development_mode) {
 
-        echo "<script>alert('this is an admin');</script>";
-    }
+        echo "<script>alert('debug mode');</script>";
 
-    if(isset($_SESSION['is_veterinarian'])) {
+        if(isset($_SESSION['is_user'])) {
 
-        echo "<script>alert('this is a veterinarian');</script>";
+            echo "<script>alert('this is a user');</script>";
+        }
+
+        if(isset($_SESSION['is_admin'])) {
+
+            echo "<script>alert('this is an admin');</script>";
+        }
+
+        if(isset($_SESSION['is_veterinarian'])) {
+
+            echo "<script>alert('this is a veterinarian');</script>";
+        }
     }
 ?>
