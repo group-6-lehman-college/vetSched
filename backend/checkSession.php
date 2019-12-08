e76dea6c12010c8e36ca04b11e21b274d016fae2
alixlm19 <alixlm19@hotmail.com>
2019-12-08 04:19:22 -0500

    Included debug functionality

--- backend/checkSession.php
+++ backend/checkSession.php
@@ -1,6 +1,22 @@
 <?php
     session_start();
+    
     if(!isset($_SESSION['token'])) {
         header("Location: ../login.php");
+    }
+    
+    if(isset($_SESSION['is_user'])) {
+
+        echo "<script>alert('this is a user');</script>";
+    }
+
+    if(isset($_SESSION['is_admin'])) {
+
+        echo "<script>alert('this is an admin');</script>";
+    }
+
+    if(isset($_SESSION['is_veterinarian'])) {
+
+        echo "<script>alert('this is a veterinarian');</script>";
     }
 ?>
